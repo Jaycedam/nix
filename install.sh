4f64f9354035fd8d157f0b0138d329bfecd7c614
Jaycedam <45575946+Jaycedam@users.noreply.github.com>
2025-12-20 13:45:46 -0300

    feat: colored output from install script

--- install.sh
+++ install.sh
@@ -6,7 +6,17 @@ TARGET_DIR="$HOME/dev/nix"
 SOURCE="$TARGET_DIR/configuration.nix"
 NIXOS_CONFIG="/etc/nixos/configuration.nix"
 
-echo "Requesting sudo access..."
+# Colors
+YELLOW='\033[1;33m'
+RED='\033[1;31m'
+GREEN='\033[1;32m'
+MAGENTA='\033[1;35m'
+NC='\033[0m' # No Color
+
+# Arrow
+ARROW="${MAGENTA}➜${NC}"
+
+echo -e "$ARROW Requesting sudo access..."
 sudo -v
 
 # Keep sudo alive until the script exits
@@ -16,33 +26,32 @@ while true; do
     kill -0 "$$" || exit
 done 2>/dev/null &
 
-echo "Cloning nix repository to $TARGET_DIR..."
 if [ -d "$TARGET_DIR" ]; then
-    echo "Directory $TARGET_DIR already exists, skipping clone."
+    echo -e "$ARROW Directory $TARGET_DIR already exists. ${YELLOW}Skipping...${NC}"
 else
+    echo -e "$ARROW Cloning repository $REPO_URL to $TARGET_DIR..."
     git clone "$REPO_URL" "$TARGET_DIR" || {
-        echo "Error: Failed to clone repository!"
+        echo -e "$ARROW ${RED}Error:${NC} Failed to clone repository!"
         exit 1
     }
 fi
 
 cd "$TARGET_DIR"
 
-echo "Checking for configuration.nix..."
+echo -e "$ARROW Checking for configuration.nix..."
 if [ ! -f "$SOURCE" ]; then
-    echo "Error: configuration.nix not found in repository"
+    echo -e "$ARROW ${RED}Error:${NC} configuration.nix not found in repository"
     exit 1
 fi
 
-echo "Creating symlink for configuration.nix..."
 if [ -L "$NIXOS_CONFIG" ] && [ "$(readlink -f "$NIXOS_CONFIG")" = "$SOURCE" ]; then
-    echo "configuration.nix already correctly symlinked"
+    echo -e "$ARROW configuration.nix already symlinked. ${YELLOW}Skipping...${NC}"
 else
-    echo "Updating configuration.nix symlink"
+    echo -e "$ARROW Creating symlink for configuration.nix..."
     sudo ln -sfv "$SOURCE" "$NIXOS_CONFIG"
 fi
 
-echo "Rebuilding NixOS configuration..."
+echo -e "$ARROW Rebuilding NixOS configuration..."
 sudo nixos-rebuild switch
 
-echo "Done."
+echo -e "$ARROW ${GREEN}Done!${NC} Reboot to apply all changes."
