a92e426e73a05950777d14d7a5160b5c88e068d8
Jaycedam <45575946+Jaycedam@users.noreply.github.com>
2025-12-25 18:47:06 -0300

    feat: keyd control keys and fn layer

--- modules/nixos/system/apps-services.nix
+++ modules/nixos/system/apps-services.nix
@@ -43,7 +43,32 @@
           l = "overloadt(meta, l, 200)";
           ";" = "overloadt(alt, ;, 200)";
 
+          # Brightness
+          f1 = "brightnessdown";
+          f2 = "brightnessup";
+
+          # Media
+          f7 = "previoussong";
+          f8 = "playpause";
+          f9 = "nextsong";
+
+          # Volume
+          f10 = "mute";
+          f11 = "volumedown";
+          f12 = "volumeup";
+
           space = "overloadt(nav_layer, space, 200)";
+        };
+
+        fn = {
+          f1 = "f1";
+          f2 = "f2";
+          f7 = "f7";
+          f8 = "f8";
+          f9 = "f9";
+          f10 = "f10";
+          f11 = "f11";
+          f12 = "f12";
         };
 
         nav_layer = {
