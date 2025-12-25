78ba0cd0da6db9fb9d0ae761b121ee48b5f93cb9
Jaycedam <45575946+Jaycedam@users.noreply.github.com>
2025-12-25 16:25:41 -0300

    fix: import paths

--- modules/common/system/scripts.nix
+++ modules/common/system/scripts.nix
@@ -2,7 +2,7 @@
 
 {
   environment.systemPackages = [
-    (import ../scripts/rofi/power-menu.nix { inherit pkgs; })
-    (import ../scripts/hypr/launch-or-focus.nix { inherit pkgs; })
+    (import ../../../scripts/rofi/power-menu.nix { inherit pkgs; })
+    (import ../../../scripts/hypr/launch-or-focus.nix { inherit pkgs; })
   ];
 }
