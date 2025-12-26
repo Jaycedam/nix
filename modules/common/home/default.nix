b38a022b18362293e922e72fec6c4350cd1bc49d
Jaycedam <45575946+Jaycedam@users.noreply.github.com>
2025-12-26 14:14:44 -0300

    feat: tmux like keybinds for kitty

--- modules/common/home/default.nix
+++ modules/common/home/default.nix
@@ -50,8 +50,35 @@
     };
     kitty = {
       enable = true;
+      enableGitIntegration = true;
+      shellIntegration.enableFishIntegration = true;
       settings = {
         cursor_trail = 1;
+      };
+      keybindings = {
+        # tmux like keybinds https://sw.kovidgoyal.net/kitty/actions/
+        # possible future actions: open_url
+
+        ## tabs
+        "ctrl+b>w" = "select_tab";
+        "ctrl+b>x" = "close_tab";
+        "ctrl+b>n>" = "next_tab";
+        "ctrl+b>p" = "previous_tab";
+        "ctrl+b>N>" = "move_tab_forward";
+        "ctrl+b>P" = "move_tab_backward";
+        "ctrl+b>comma" = "set_tab_title";
+        "ctrl+b>c" = "new_tab_with_cwd";
+        "ctrl+b>t" = "toggle_tab";
+
+        ## windows (splits)
+        "ctrl+b>s" = "new_window_with_cwd";
+
+        ## actions
+        "ctrl+b>[" = "show_scrollback";
+        "ctrl+b>r" = "load_config_file";
+
+        # custom lauch commands
+        "ctrl+b>g" = "launch --cwd=current --type=tab 'lazygit'";
       };
     };
     lazygit = {
