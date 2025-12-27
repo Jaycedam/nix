9c566eceb479e6158ae69a2f0372cb5e4e417ba0
Jaycedam <45575946+Jaycedam@users.noreply.github.com>
2025-12-27 17:58:44 -0300

    feat: nix-darwin config

--- darwin/configuration.nix
+++ darwin/configuration.nix
@@ -8,6 +8,61 @@
     shell = pkgs.fish;
   };
 
+  system = {
+    primaryUser = "jay";
+    keyboard = {
+      enableKeyMapping = true;
+      remapCapsLockToEscape = true;
+    };
+    defaults = {
+      screensaver.askForPasswordDelay = 3;
+      screencapture.location = "~/Pictures/screenshots";
+      controlcenter.BatteryShowPercentage = true;
+      menuExtraClock.ShowAMPM = false;
+      NSGlobalDomain = {
+        #_HIHideMenuBar = true;
+        AppleIconAppearanceTheme = "RegularDark";
+        AppleInterfaceStyle = "Dark";
+        NSAutomaticSpellingCorrectionEnabled = false;
+        InitialKeyRepeat = 2;
+        KeyRepeat = 25;
+      };
+      WindowManager.EnableStandardClickToShowDesktop = false;
+      finder = {
+        ShowStatusBar = true;
+        _FXSortFoldersFirst = true;
+        _FXShowPosixPathInTitle = true;
+        AppleShowAllExtensions = true;
+        AppleShowAllFiles = true;
+        CreateDesktop = false;
+        FXDefaultSearchScope = "SCcf";
+        FXPreferredViewStyle = "clmv";
+        NewWindowTarget = "Home";
+        ShowExternalHardDrivesOnDesktop = false;
+        ShowRemovableMediaOnDesktop = false;
+        ShowPathbar = true;
+      };
+      dock = {
+        autohide = true;
+        autohide-delay = 0.1;
+        mru-spaces = false;
+        show-recents = false;
+        showhidden = true;
+        static-only = true;
+        wvous-br-corner = 1;
+        wvous-tl-corner = 1;
+        wvous-tr-corner = 1;
+        wvous-bl-corner = 1;
+      };
+    };
+
+  };
+  networking = {
+    hostName = "mac";
+    computerName = "mac";
+    localHostName = "mac";
+    # dns = [ "1.1.1.1" "1.0.0.1" ];
+  };
 
   programs.fish = {
     enable = true;
