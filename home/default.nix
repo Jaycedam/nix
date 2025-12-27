e97cb11265c19256f1b1a54e5e9f46748668f6dc
Jaycedam <45575946+Jaycedam@users.noreply.github.com>
2025-12-27 13:43:33 -0300

    refactor: move home configs to nix files

--- home/default.nix
+++ home/default.nix
@@ -3,29 +3,37 @@
   ...
 }:
 {
-  home.username = "jay";
-  home.homeDirectory = "/home/jay";
-  home.pointerCursor = {
-    enable = true;
-    package = pkgs.bibata-cursors;
-    name = "Bibata-Modern-Ice";
-    size = 24;
-
-    gtk.enable = true;
-    x11 = {
-      defaultCursor = "Bibata-Modern-Ice";
+  # home-manager configuration
+  home = {
+    username = "jay";
+    homeDirectory = "/home/jay";
+    pointerCursor = {
       enable = true;
+      package = pkgs.bibata-cursors;
+      name = "Bibata-Modern-Ice";
+      size = 24;
+
+      gtk.enable = true;
+      x11 = {
+        defaultCursor = "Bibata-Modern-Ice";
+        enable = true;
+      };
     };
+
+    # don't change this
+    stateVersion = "25.11";
   };
 
-  # large configs go to ./programs/
-  # consider move them all?
+  # large configs go to separate files
   imports = [
     ./hyprland.nix
     ./waybar.nix
+    ./hyprlock.nix
+    ./hypridle.nix
     ./neovim/default.nix
     ./helix.nix
     ./tmux.nix
+    ./kitty.nix
   ];
 
   services = {
@@ -37,54 +45,14 @@
         wallpaper = ", ${../wallpapers/1.jpg}";
       };
     };
-    hypridle = {
-      enable = true;
-      settings = {
-        general = {
-          lock_cmd = "pidof hyprlock || hyprlock"; # avoid starting multiple hyprlock instances.
-          before_sleep_cmd = "loginctl lock-session"; # lock before suspend.
-          after_sleep_cmd = "hyprctl dispatch dpms on"; # to avoid having to press a key twice to turn on the display.
-        };
-
-        listener = [
-          {
-            timeout = "150";
-            on-timeout = "brightnessctl -s set 10"; # set monitor backlight to minimum, avoid 0 on OLED monitor.
-            on-resume = "brightnessctl -r"; # monitor backlight restore.
-          }
-
-          # turn off keyboard backlight, comment out this section if you dont have a keyboard backlight.
-          {
-            timeout = 150;
-            on-timeout = "brightnessctl -sd rgb:kbd_backlight set 0";
-            on-resume = "brightnessctl -rd rgb:kbd_backlight";
-          }
-
-          {
-            timeout = 300;
-            on-timeout = "loginctl lock-session"; # lock screen when timeout has passed
-          }
-
-          {
-            timeout = 330;
-            on-timeout = "hyprctl dispatch dpms off"; # screen off when timeout has passed
-            on-resume = "hyprctl dispatch dpms on && brightnessctl -r"; # screen on when activity is detected after timeout has fired.
-          }
-
-          {
-            timeout = 1800;
-            on-timeout = "systemctl suspend"; # suspend pc
-          }
-        ];
-      };
-    };
     dunst = {
       enable = true;
       settings = {
         global = {
           width = "(200,300)";
-          offset = "(30,50)";
+          offset = "(10,10)";
           origin = "top-right";
+          frame_width = 1;
           corner_radius = 5;
         };
       };
@@ -99,26 +67,6 @@
     rofi = {
       enable = true;
       # todo= increase font and remove table cells
-    };
-    hyprlock = {
-      enable = true;
-      settings = {
-        general = {
-          hide_cursor = true;
-        };
-
-        animations = {
-          enabled = true;
-          fade_in = {
-            duration = 300;
-            bezier = "easeOutQuint";
-          };
-          fade_out = {
-            duration = 300;
-            bezier = "easeOutQuint";
-          };
-        };
-      };
     };
 
     git = {
@@ -155,41 +103,6 @@
       enable = true;
       enableFishIntegration = true;
     };
-    kitty = {
-      enable = true;
-      enableGitIntegration = true;
-      shellIntegration.enableFishIntegration = true;
-      settings = {
-        cursor_trail = 1;
-      };
-      keybindings = {
-        # tmux like keybinds https://sw.kovidgoyal.net/kitty/actions/
-        # possible future actions: open_url
-
-        ## tabs
-        "ctrl+t>w" = "select_tab";
-        "ctrl+t>x" = "close_tab";
-        "ctrl+t>n>" = "next_tab";
-        "ctrl+t>p" = "previous_tab";
-        "ctrl+t>N>" = "move_tab_forward";
-        "ctrl+t>P" = "move_tab_backward";
-        "ctrl+t>comma" = "set_tab_title";
-        "ctrl+t>c" = "new_tab_with_cwd";
-        "ctrl+t>t" = "toggle_tab";
-
-        ## windows (splits)
-        "ctrl+t>s" = "new_window_with_cwd";
-
-        ## actions
-        "ctrl+t>[" = "show_scrollback";
-        "ctrl+t>r" = "load_config_file";
-
-        # custom lauch commands
-        "ctrl+t>g" = "launch --cwd=current --type=tab 'lazygit'";
-        "ctrl+t>a" = "launch --cwd=current --type=tab 'opencode'";
-        "ctrl+t>b" = "launch 'gh browse > /dev/null'";
-      };
-    };
     lazygit = {
       enable = true;
       enableFishIntegration = true;
@@ -203,6 +116,4 @@
     opencode.enable = true;
   };
 
-  # don't change this
-  home.stateVersion = "25.11";
 }
---
+++ home/kitty.nix
@@ -0,0 +1,38 @@
+{ ... }:
+{
+  programs.kitty = {
+    enable = true;
+    enableGitIntegration = true;
+    shellIntegration.enableFishIntegration = true;
+    settings = {
+      cursor_trail = 1;
+    };
+    keybindings = {
+      # tmux like keybinds https://sw.kovidgoyal.net/kitty/actions/
+      # possible future actions: open_url
+
+      ## tabs
+      "ctrl+t>w" = "select_tab";
+      "ctrl+t>x" = "close_tab";
+      "ctrl+t>n>" = "next_tab";
+      "ctrl+t>p" = "previous_tab";
+      "ctrl+t>N>" = "move_tab_forward";
+      "ctrl+t>P" = "move_tab_backward";
+      "ctrl+t>comma" = "set_tab_title";
+      "ctrl+t>c" = "new_tab_with_cwd";
+      "ctrl+t>t" = "toggle_tab";
+
+      ## windows (splits)
+      "ctrl+t>s" = "new_window_with_cwd";
+
+      ## actions
+      "ctrl+t>[" = "show_scrollback";
+      "ctrl+t>r" = "load_config_file";
+
+      # custom lauch commands
+      "ctrl+t>g" = "launch --cwd=current --type=tab 'lazygit'";
+      "ctrl+t>a" = "launch --cwd=current --type=tab 'opencode'";
+      "ctrl+t>b" = "launch 'gh browse > /dev/null'";
+    };
+  };
+}
