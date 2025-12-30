b01babf5646312687b1506cd3db05a0f42f086a4
Jaycedam <45575946+Jaycedam@users.noreply.github.com>
2025-12-30 00:58:43 -0300

    chore: disable kitty keybinds

--- home/kitty.nix
+++ home/kitty.nix
@@ -12,32 +12,5 @@
       hide_window_decorations = "titlebar-only"; # darwin only option
 
     };
-    keybindings = {
-      # tmux like keybinds https://sw.kovidgoyal.net/kitty/actions/
-      # possible future actions: open_url
-
-      ## tabs
-      "ctrl+t>w" = "select_tab";
-      "ctrl+t>x" = "close_tab";
-      "ctrl+t>n>" = "next_tab";
-      "ctrl+t>p" = "previous_tab";
-      "ctrl+t>N>" = "move_tab_forward";
-      "ctrl+t>P" = "move_tab_backward";
-      "ctrl+t>comma" = "set_tab_title";
-      "ctrl+t>c" = "new_tab_with_cwd";
-      "ctrl+t>t" = "toggle_tab";
-
-      ## windows (splits)
-      "ctrl+t>s" = "new_window_with_cwd";
-
-      ## actions
-      "ctrl+t>[" = "show_scrollback";
-      "ctrl+t>r" = "load_config_file";
-
-      # custom lauch commands
-      "ctrl+t>g" = "launch --cwd=current --type=tab 'lazygit'";
-      "ctrl+t>a" = "launch --cwd=current --type=tab 'opencode'";
-      "ctrl+t>b" = "launch 'gh browse > /dev/null'";
-    };
   };
 }
