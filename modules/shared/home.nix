ae4285ec3afebb8ab314e8a9a6d23676cfd2df98
Jaycedam <45575946+Jaycedam@users.noreply.github.com>
2025-12-30 23:31:14 -0300

    feat(fish): transient prompt
    first draft

--- modules/shared/home.nix
+++ modules/shared/home.nix
@@ -11,8 +11,23 @@
     btop.enable = true;
     fish = {
       enable = true;
+      functions = {
+        fish_prompt = ''
+          set -l host (prompt_login)
+          set -l pwd (set_color cyan)(prompt_pwd)
+          set -l git (set_color yellow)(fish_git_prompt " %s")
+          set -l icon (set_color blue)"❯ "
+
+          if contains -- --final-rendering $argv
+            string join "" -- $icon
+          else
+            string join "" -- $host " " $pwd $git " " $icon
+          end
+        '';
+      };
       interactiveShellInit = ''
         set fish_greeting ""
+        set -g fish_transient_prompt 1
       '';
       shellAbbrs = {
         v = "nvim";
