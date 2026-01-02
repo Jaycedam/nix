bd62ad37557a555d8a2c44afa403b6e832bb68a2
Jaycedam <45575946+Jaycedam@users.noreply.github.com>
2026-01-02 14:58:10 -0300

    feat: DoH
    fixes #25

--- modules/nixos/options.nix
+++ modules/nixos/options.nix
@@ -72,10 +72,42 @@
   hardware.i2c.enable = true;
 
   # Enable networking
-  networking.wireless.iwd.enable = true;
+  networking = {
+    wireless.iwd.enable = true;
 
-  networking.networkmanager = {
-    enable = true;
-    wifi.backend = "iwd";
+    networkmanager = {
+      enable = true;
+      wifi.backend = "iwd";
+      dns = "none"; # we are using dnsproxy for the dns
+    };
+
+    nameservers = [
+      "127.0.0.1"
+      "::1"
+    ];
   };
+
+  services.dnscrypt-proxy = {
+    enable = true;
+    settings = {
+      sources.public-resolvers = {
+        urls = [
+          "https://raw.githubusercontent.com/DNSCrypt/dnscrypt-resolvers/master/v3/public-resolvers.md"
+          "https://download.dnscrypt.info/resolvers-list/v3/public-resolvers.md"
+        ];
+        minisign_key = "RWQf6LRCGA9i53mlYecO4IzT51TGPpvWucNSCh1CBM0QTaLn73Y7GFO3";
+        cache_file = "/var/lib/dnscrypt-proxy/public-resolvers.md";
+      };
+
+      require_dnssec = true;
+      require_nolog = true;
+      require_nofilter = true;
+
+      server_names = [
+        "mullvad-adblock-doh"
+        "adguard-dns-doh"
+      ];
+    };
+  };
+  systemd.services.dnscrypt-proxy.serviceConfig.StateDirectory = "dnscrypt-proxy";
 }
