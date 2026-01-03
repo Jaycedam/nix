b0ec556dc877f5c82989edc1adc1b457652f965d
Jaycedam <45575946+Jaycedam@users.noreply.github.com>
2026-01-03 01:18:43 -0300

    feat: use dms-shell ipc for external monitor brightness
    since I'm no longer using the custom script I'm removing it.

--- modules/nixos/scripts.nix
+++ modules/nixos/scripts.nix
@@ -4,6 +4,5 @@
   environment.systemPackages = [
     (import ../../scripts/rofi/power-menu.nix { inherit pkgs; })
     (import ../../scripts/hypr/launch-or-focus.nix { inherit pkgs; })
-    (import ../../scripts/system/brightness.nix { inherit pkgs; })
   ];
 }
