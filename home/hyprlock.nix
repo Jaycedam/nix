b1cc676739df9aa6188e6bf1fbd81cc7972865cb
Jaycedam <45575946+Jaycedam@users.noreply.github.com>
2026-01-08 02:21:52 -0300

    chore: hyprlock theme

--- home/hyprlock.nix
+++ home/hyprlock.nix
@@ -3,21 +3,59 @@
   programs.hyprlock = {
     enable = true;
     settings = {
-      general = {
-        hide_cursor = true;
+      # background = {
+      #   monitor = "";
+      #   path = " screenshot";
+      #   blur_passes = 3;
+      #   blur_size = 10;
+      # };
+
+      input-field = {
+        monitor = "";
+        size = "300, 30";
+        outline_thickness = 1;
+        dots_size = 0.25;
+        dots_spacing = 0.55;
+        dots_center = true;
+        dots_rounding = -1;
+        fade_on_empty = false;
+        placeholder_text = "";
+        hide_input = false;
+        fail_text = "$FAIL <b>($ATTEMPTS)</b>";
+        fail_transition = 300;
+        invert_numlock = false;
+        position = "0, -468";
+        halign = "center";
+        valign = "center";
       };
 
-      animations = {
-        enabled = true;
-        fade_in = {
-          duration = 300;
-          bezier = "easeOutQuint";
-        };
-        fade_out = {
-          duration = 300;
-          bezier = "easeOutQuint";
-        };
-      };
+      label = [
+        {
+          monitor = "";
+          text = "cmd[update:1000] echo \"$(date +\"%A, %B %d\")\"";
+          font_size = 20;
+          position = "0, 405";
+          halign = " center";
+          valign = " center";
+        }
+        {
+          monitor = "";
+          text = "cmd[update:1000] echo \"$(date +\"%k:%M\")\"";
+          font_size = 93;
+          position = "0, 310";
+          halign = " center";
+          valign = " center";
+        }
+        {
+          monitor = "";
+          text = "Enter Password";
+          font_size = 10;
+          position = "0, -438";
+          halign = " center";
+          valign = " center";
+        }
+      ];
+
     };
   };
 }
