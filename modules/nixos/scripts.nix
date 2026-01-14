91b91a583015695e61da8733e98ba2bdb63984e2
Jaycedam <45575946+Jaycedam@users.noreply.github.com>
2026-01-14 15:29:55 -0300

    feat: launch-webapp and launch-or-focus-webapps
    The generic launch-webapp is the base used in desktop entries and is
    also used in the launch-or-focus, which checks if an instance is open to
    focus on it first. Launch or focus logic is niri specific which is why
    it's on the niri dir.

--- modules/nixos/scripts.nix
+++ modules/nixos/scripts.nix
@@ -7,5 +7,7 @@
     (import ../../scripts/system/volume.nix { inherit pkgs; })
     (import ../../scripts/niri/launch-or-focus.nix { inherit pkgs; })
     (import ../../scripts/programs/nvim-wrapper.nix { inherit pkgs; })
+    (import ../../scripts/system/launch-webapp.nix { inherit pkgs; })
+    (import ../../scripts/niri/launch-or-focus-webapp.nix { inherit pkgs; })
   ];
 }
