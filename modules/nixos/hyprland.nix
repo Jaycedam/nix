6c757d06cf95cd0898dc64a56a1ba90a93a7ba57
Jaycedam <45575946+Jaycedam@users.noreply.github.com>
2026-01-16 22:22:59 -0300

    chore: removed options for hypr and niri, that were already default

--- modules/nixos/hyprland.nix
+++ modules/nixos/hyprland.nix
@@ -1,20 +1,5 @@
-{ pkgs, ... }:
-{
-  programs = {
-    hyprland = {
-      enable = true;
-      xwayland.enable = true;
-    };
-  };
-
-  # enable extra xdg-portal-hyprland and gtk for file picker
-  xdg.portal = {
-    enable = true;
-    extraPortals = with pkgs; [
-      xdg-desktop-portal-hyprland
-      xdg-desktop-portal-gtk
-    ];
-  };
+_: {
+  programs.hyprland.enable = true;
 
   environment.loginShellInit = ''
     if [ -z "$DISPLAY" ] && [ "$(tty)" = "/dev/tty1" ]; then
--- modules/nixos/niri.nix
+++ modules/nixos/niri.nix
@@ -7,13 +7,4 @@
   programs = {
     niri.enable = true;
   };
-
-  # enable extra xdg-portal-hyprland and gtk for file picker
-  xdg.portal = {
-    enable = true;
-    extraPortals = with pkgs; [
-      xdg-desktop-portal-gtk # implements most of the basic functionality, this is the "default fallback portal".
-      xdg-desktop-portal-gnome # required for screencasting support
-    ];
-  };
 }
