b2276bce2acd25c2e0aba29bc5cba3c4e3fb2c4b
Jaycedam <45575946+Jaycedam@users.noreply.github.com>
2026-01-19 01:00:50 -0300

    refactor: simplify pre matugen file creation

--- home/matugen/default.nix
+++ home/matugen/default.nix
@@ -6,43 +6,27 @@
   };
 
   home.activation.createMatugenColors = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
-    config_dir="${config.xdg.configHome}"
-    mkdir -p "$config_dir/hypr" \
-             "$config_dir/waybar" \
-             "$config_dir/kitty" \
-             "$config_dir/fuzzel" \
-             "$config_dir/gtk-3.0" \
-             "$config_dir/gtk-4.0" \
-             "$config_dir/mako" \
-             "$config_dir/yazi" \
-          "$config_dir/btop/themes" \
-          "$config_dir/neovim" \
-          "$config_dir/tmux" \
-          "$config_dir/ghostty/themes" \
-          "$config_dir/niri"
-    touch "$config_dir/hypr/colors.conf" \
-          "$config_dir/waybar/colors.css" \
-          "$config_dir/kitty/colors.conf" \
-          "$config_dir/gtk-3.0/colors.css" \
-          "$config_dir/gtk-4.0/colors.css" \
-          "$config_dir/mako/mako-colors" \
-          "$config_dir/yazi/theme.toml" \
-          "$config_dir/btop/themes/matugen.theme" \
-          "$config_dir/neovim/generated.lua" \
-          "$config_dir/tmux/generated.conf" \
-          "$config_dir/ghostty/themes/matugen" \
-          "$config_dir/niri/colors.kdl"
-    touch "$config_dir/hypr/colors.conf" \
-          "$config_dir/waybar/colors.css" \
-          "$config_dir/kitty/colors.conf" \
-          "$config_dir/fuzzel/colors.ini" \
-          "$config_dir/gtk-3.0/colors.css" \
-          "$config_dir/gtk-4.0/colors.css" \
-          "$config_dir/yazi/theme.toml" \
-          "$config_dir/btop/themes/matugen.theme" \
-          "$config_dir/nvim/generated.lua" \
-          "$config_dir/tmux/generated.conf" \
-          "$config_dir/ghostty/themes/matugen" \
-          "$config_dir/niri/colors.kdl"
+    conf_dir="${config.xdg.configHome}"
+
+    colorFiles=(
+      "$conf_dir/hypr/colors.conf"
+      "$conf_dir/waybar/colors.css"
+      "$conf_dir/kitty/colors.conf"
+      "$conf_dir/fuzzel/colors.ini"
+      "$conf_dir/gtk-3.0/colors.css"
+      "$conf_dir/gtk-4.0/colors.css"
+      "$conf_dir/mako/mako-colors"
+      "$conf_dir/yazi/theme.toml"
+      "$conf_dir/btop/themes/matugen.theme"
+      "$conf_dir/nvim/generated.lua"
+      "$conf_dir/tmux/generated.conf"
+      "$conf_dir/ghostty/themes/matugen"
+      "$conf_dir/niri/colors.kdl"
+    )
+
+    for file in "''${colorFiles[@]}"; do
+      mkdir -p "$(dirname "$file")"
+      touch "$file"
+    done
   '';
 }
