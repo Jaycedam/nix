c2ef46040b8ddc6b6b8ab0b953dfa01dcab54624
Jaycedam <45575946+Jaycedam@users.noreply.github.com>
2026-01-22 02:40:31 -0300

    chore: added foot terminal

--- home/programs.nix
+++ home/programs.nix
@@ -1,68 +1,77 @@
 { pkgs, lib, ... }:
 {
-  home.packages = with pkgs; [
-    # cli
-    fwupd # upgrade firmware
-    playerctl
-    brightnessctl
-    impala # wifi tui selector
-    wl-clipboard
-    libnotify
-    ddcutil # ext monitor brightness
-    wiremix
-    tree
-    pstree
-    trash-cli
-    tldr
-    ripgrep
-    yt-dlp
-    fd
-    jq
-    imagemagick
-    fastfetch
-    mpv
-    bitwarden-cli
-    ffmpeg
+  home.packages =
+    with pkgs;
+    [
+      # cli
+      fwupd # upgrade firmware
+      playerctl
+      brightnessctl
+      impala # wifi tui selector
+      wl-clipboard
+      libnotify
+      ddcutil # ext monitor brightness
+      wiremix
+      tree
+      pstree
+      trash-cli
+      tldr
+      ripgrep
+      yt-dlp
+      fd
+      jq
+      imagemagick
+      fastfetch
+      mpv
+      bitwarden-cli
+      ffmpeg
 
-    # desktop apps
-    pavucontrol
-    localsend
-    nautilus # needed by niri
-    gimp
-    libreoffice-fresh
-    signal-desktop
-    ente-desktop
-    # grayjay
-    seahorse # gnome keyring manager
-    transmission_4-gtk
-    bitwarden-desktop
-    spotube
+      # desktop apps
+      pavucontrol
+      localsend
+      nautilus # needed by niri
+      gimp
+      libreoffice-fresh
+      signal-desktop
+      ente-desktop
+      # grayjay
+      seahorse # gnome keyring manager
+      transmission_4-gtk
+      bitwarden-desktop
+      spotube
 
-    ### browser ###
-    brave
+      ### browser ###
+      brave
 
-    ### dev ###
-    # tools
-    bun
-    tree-sitter
-    lazygit
-    nodejs_24
-    android-tools
-    # languages
-    go
+      ### dev ###
+      # tools
+      bun
+      tree-sitter
+      lazygit
+      nodejs_24
+      android-tools
+      # languages
+      go
 
-    # linters
-    eslint
-    shellcheck
-    statix
-  ] ++ lib.optionals (builtins.elem pkgs.stdenv.hostPlatform.system (proton-pass.meta.platforms or [])) [
-    proton-pass
-  ] ++ lib.optionals (builtins.elem pkgs.stdenv.hostPlatform.system (cryptomator.meta.platforms or [])) [
-    cryptomator
-  ] ++ lib.optionals (builtins.elem pkgs.stdenv.hostPlatform.system (docker.meta.platforms or [])) [
-    docker
-    docker-compose
-  ];
+      # linters
+      eslint
+      shellcheck
+      statix
+    ]
+    ++
+      lib.optionals (builtins.elem pkgs.stdenv.hostPlatform.system (proton-pass.meta.platforms or [ ]))
+        [
+          proton-pass
+        ]
+    ++
+      lib.optionals (builtins.elem pkgs.stdenv.hostPlatform.system (cryptomator.meta.platforms or [ ]))
+        [
+          cryptomator
+        ]
+    ++ lib.optionals (builtins.elem pkgs.stdenv.hostPlatform.system (docker.meta.platforms or [ ])) [
+      docker
+      docker-compose
+    ];
 
   programs = {
     btop = {
@@ -76,6 +85,8 @@
       enable = true;
       settings.theme = "system";
     };
+
+    foot.enable = true;
 
     zoxide = {
       enable = true;
