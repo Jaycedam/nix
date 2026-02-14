8a38b4f8c270122e40211e610b8afb7d6c6ed519
jaycem <45575946+jaycem-dev@users.noreply.github.com>
2026-02-14 02:44:10 -0300

    chore: simplify waybar config

--- modules/home/waybar/waybar.nix
+++ modules/home/waybar/waybar.nix
@@ -1,232 +1,209 @@
-{
-  lib,
-  ...
-}:
-{
+_: {
   programs.waybar = {
     enable = true;
     systemd.enable = true;
-    settings = lib.mkMerge [
-      {
-        mainBar = {
-          layer = "top";
-          position = "bottom";
-          spacing = 10;
-          margin = "0 10 5 10";
+    settings = {
+      mainBar = {
+        layer = "top";
+        position = "bottom";
+        spacing = 10;
+        # margin = "0 10 5 10";
 
-          modules-left = [
-            "group/actions"
-            "niri/window"
+        modules-left = [
+          "niri/workspaces"
+          "niri/window"
+        ];
+        modules-center = [
+          "clock"
+        ];
+        modules-right = [
+          "mpris"
+          "privacy"
+          "group/tray-expander"
+          "group/system"
+        ];
+
+        "group/system" = {
+          orientation = "horizontal";
+          modules = [
+            "network"
+            "bluetooth"
+            "battery"
+            "pulseaudio"
           ];
-          modules-center = [
-            "niri/workspaces"
+        };
+
+        "group/tray-expander" = {
+          "orientation" = "inherit";
+          "drawer" = {
+            transition-duration = 600;
+            children-class = "tray-group-item";
+            transition-left-to-right = false;
+          };
+          modules = [
+            "custom/expand-icon"
+            "idle_inhibitor"
+            "tray"
           ];
-          modules-right = [
-            "mpris"
-            "privacy"
-            "group/system"
-            "clock"
-          ];
+        };
+        "custom/expand-icon" = {
+          format = "";
+          tooltip = false;
+          on-scroll-up = "";
+          on-scroll-down = "";
+          on-scroll-left = "";
+          on-scroll-right = "";
+        };
 
-          "group/system" = {
-            orientation = "horizontal";
-            modules = [
-              "network"
-              "bluetooth"
-              "battery"
-              "pulseaudio"
-            ];
-          };
-
-          "group/tray-expander" = {
-            "orientation" = "inherit";
-            "drawer" = {
-              transition-duration = 600;
-              children-class = "tray-group-item";
-              transition-left-to-right = false;
-            };
-            modules = [
-              "custom/expand-icon"
-              "tray"
-            ];
-          };
-          "custom/expand-icon" = {
-            format = "";
-            tooltip = false;
-            on-scroll-up = "";
-            on-scroll-down = "";
-            on-scroll-left = "";
-            on-scroll-right = "";
-          };
-
-          "hyprland/workspaces" = {
-            format = "{icon}";
-            format-icons = {
-              active = "";
-              persistent = "";
-              empty = "";
-            };
-            persistent-workspaces = {
-              "*" = 5;
-            };
-          };
-
-          "hyprland/window" = {
-            format = "{title}";
-            max-length = 30;
-            icon = true;
-            icon-size = 16;
-          };
-
-          "niri/workspaces" = {
-            format = "{icon}";
-            format-icons = {
-              browser = "";
-              dev = "";
-              media = "";
-              communication = "";
-              default = "";
-            };
-          };
-
-          "niri/window" = {
-            format = "{title}";
-            max-length = 30;
-            icon = true;
-            icon-size = 16;
-          };
-
-          privacy = {
-            icon-spacing = 4;
-            icon-size = 18;
-            transition-duration = 250;
-            modules = [
-              {
-                type = "screenshare";
-                tooltip = true;
-                tooltip-icon-size = 24;
-              }
-              {
-                type = "audio-in";
-                tooltip = true;
-                tooltip-icon-size = 24;
-              }
-            ];
-            ignore-monitor = true;
-            ignore = [
-              {
-                type = "audio-in";
-                name = "cava";
-              }
-            ];
-          };
-
-          mpris = {
-            format = "{player_icon} {dynamic}";
-            format-paused = "{status_icon} {dynamic}";
-            dynamic-order = [
-              "title"
-              "artist"
-            ];
-            dynamic-len = 40;
-            player-icons = {
-              default = "";
-            };
-            status-icons = {
-              paused = "";
-            };
-          };
-
-          "group/actions" = {
-            orientation = "inherit";
-            modules = [
-              "custom/power"
-              "idle_inhibitor"
-              "group/tray-expander"
-            ];
-          };
-          "custom/power" = {
-            format = "";
-            tooltip = false;
-            on-click = "dmenu-power";
-          };
-
-          network = {
-            format-wifi = "";
-            format-ethernet = "";
-            tooltip-format = "Connected to {essid}";
-            format-linked = "󱘖 {ifname} (No IP)";
-            format-disconnected = " Disconnected";
-            interval = 3;
-            on-click = "kitty --class 'impala' impala";
-          };
-
-          battery = {
-            states = {
-              warning = 30;
-              critical = 15;
-            };
-            format = "{icon} {capacity}%";
-            format-charging = " {capacity}%";
-            interval = 1;
-            format-icons = [
-              ""
-              ""
-              ""
-              ""
-              ""
-            ];
-            tooltip = true;
-          };
-          pulseaudio = {
-            format = "{icon} {volume}%";
-            format-muted = " 0%";
-            format-icons = {
-              headphone = "";
-              hands-free = "";
-              headset = "";
-              phone = "";
-              portable = "";
-              default = [
-                ""
-                ""
-                ""
-              ];
-            };
-            on-click = "pavucontrol -t 3";
-            on-click-right = "pactl --set-sink-mute 0 toggle";
-          };
-          idle_inhibitor = {
-            format = "{icon}";
-            format-icons = {
-              activated = "";
-              deactivated = "";
-            };
-          };
-          clock = {
-            interval = 1;
-            format = "{:%a, %b %d  %H:%M}";
-            on-click = "niri-launch-or-focus-webapp calendar.proton.me";
-            tooltip = false;
-          };
-          tray = {
-            icon-size = 16;
-            spacing = 5;
-          };
-          bluetooth = {
-            format = "";
-            format-off = "";
-            format-no-controller = "";
-            format-connected = "";
-            tooltip-format = "{controller_alias}\n\n{num_connections} connected";
-            tooltip-format-connected = "{controller_alias}\n\n{num_connections} connected\n\n{device_enumerate}";
-            tooltip-format-enumerate-connected = "{device_alias}";
-            tooltip-format-enumerate-connected-battery = "{device_alias}\t{device_battery_percentage}%";
-            on-click = "kitty --class 'bluetui' bluetui";
+        "niri/workspaces" = {
+          format = "{icon}";
+          format-icons = {
+            browser = "";
+            dev = "";
+            media = "";
+            communication = "";
+            default = "";
           };
         };
-      }
-    ];
+
+        "niri/window" = {
+          format = "{title}";
+          max-length = 30;
+          icon = true;
+          icon-size = 16;
+        };
+
+        privacy = {
+          icon-spacing = 4;
+          icon-size = 18;
+          transition-duration = 250;
+          modules = [
+            {
+              type = "screenshare";
+              tooltip = true;
+              tooltip-icon-size = 24;
+            }
+            {
+              type = "audio-in";
+              tooltip = true;
+              tooltip-icon-size = 24;
+            }
+          ];
+          ignore-monitor = true;
+          ignore = [
+            {
+              type = "audio-in";
+              name = "cava";
+            }
+          ];
+        };
+
+        mpris = {
+          format = "{player_icon} {dynamic}";
+          format-paused = "{status_icon} {dynamic}";
+          dynamic-order = [
+            "title"
+            "artist"
+          ];
+          dynamic-len = 40;
+          player-icons = {
+            default = "";
+          };
+          status-icons = {
+            paused = "";
+          };
+        };
+
+        network = {
+          format-wifi = "";
+          format-ethernet = "";
+          tooltip-format = "Connected to {essid}";
+          format-linked = "󱘖 {ifname} (No IP)";
+          format-disconnected = " Disconnected";
+          interval = 3;
+          on-click = "kitty --class 'impala' impala";
+        };
+
+        battery = {
+          states = {
+            warning = 30;
+            critical = 15;
+          };
+          format = "{icon} {capacity}%";
+          format-charging = " {capacity}%";
+          interval = 1;
+          format-icons = [
+            ""
+            ""
+            ""
+            ""
+            ""
+          ];
+          tooltip = true;
+        };
+        pulseaudio = {
+          format = "{icon} {volume}%";
+          format-muted = " 0%";
+          format-icons = {
+            headphone = "";
+            hands-free = "";
+            headset = "";
+            phone = "";
+            portable = "";
+            default = [
+              ""
+              ""
+              ""
+            ];
+          };
+          on-click = "pavucontrol -t 3";
+          on-click-right = "pactl --set-sink-mute 0 toggle";
+        };
+        idle_inhibitor = {
+          format = "{icon}";
+          format-icons = {
+            activated = "";
+            deactivated = "";
+          };
+        };
+        clock = {
+          interval = 1;
+          format = "{:%A %H:%M}";
+          on-click = "niri-launch-or-focus-webapp calendar.proton.me";
+          "tooltip-format" = "<tt>{calendar}</tt>";
+          calendar = {
+            "format" = {
+              "months" = "<span color='#ffead3'><b>{}</b></span>";
+              "days" = "<span color='#ecc6d9'><b>{}</b></span>";
+              "weeks" = "<span color='#99ffdd'><b>W{}</b></span>";
+              "weekdays" = "<span color='#ffcc66'><b>{}</b></span>";
+              "today" = "<span color='#ff6699'><b><u>{}</u></b></span>";
+            };
+          };
+          "actions" = {
+            "on-click-right" = "shift_reset";
+            "on-scroll-up" = "shift_up";
+            "on-scroll-down" = "shift_down";
+          };
+        };
+        tray = {
+          icon-size = 16;
+          spacing = 5;
+        };
+        bluetooth = {
+          format = "";
+          format-off = "";
+          format-no-controller = "";
+          format-connected = "";
+          tooltip-format = "{controller_alias}\n\n{num_connections} connected";
+          tooltip-format-connected = "{controller_alias}\n\n{num_connections} connected\n\n{device_enumerate}";
+          tooltip-format-enumerate-connected = "{device_alias}";
+          tooltip-format-enumerate-connected-battery = "{device_alias}\t{device_battery_percentage}%";
+          on-click = "kitty --class 'bluetui' bluetui";
+        };
+      };
+    };
+
     style = ''
         @import "colors.css";
 
@@ -236,7 +213,7 @@
         }
 
         window#waybar {
-          background-color: transparent;
+          background-color: @background;
           color: @on_background;
         }
 
@@ -266,18 +243,6 @@
 
         #privacy {
             color: @tertiary;
-        }
-
-        #workspaces, 
-        #system, 
-        #window,
-        #actions, 
-        #privacy,
-        #clock,
-        #mpris {
-            background-color: @background;
-            border-radius: 10;
-            border: 1px solid @outline_variant;
         }
 
       #workspaces button {
