e5e01b5bd30de365dcd6b71ab9a845e02710392c
jaycem <45575946+jaycem-dev@users.noreply.github.com>
2026-02-18 00:58:27 -0300

    feat: experimental nixvim performance options

--- modules/home/nixvim/default.nix
+++ modules/home/nixvim/default.nix
@@ -10,6 +10,22 @@
     defaultEditor = true;
     viAlias = true;
     vimAlias = true;
+
+    performance = {
+      # compiles lua to bytecode for faster startup
+      byteCompileLua = {
+        enable = true;
+        configs = true;
+        initLua = true;
+        luaLib = true;
+        nvimRuntime = true;
+        plugins = true;
+
+        # combines all plugins into one file for faster startup
+        combinePlugins.enable = true;
+      };
+    };
+
     globals = {
       mapleader = " ";
     };
