63b2c0587c18c3972237e3a0ac0d00facd144668
jaycem <45575946+jaycem-dev@users.noreply.github.com>
2026-02-20 16:10:42 -0300

    style: manual stylix theme

--- modules/common/stylix.nix
+++ modules/common/stylix.nix
@@ -3,21 +3,32 @@
   stylix = {
     enable = true;
     polarity = "dark";
-    base16Scheme = "${pkgs.base16-schemes}/share/themes/rose-pine.yaml";
+    # base16Scheme = "${pkgs.base16-schemes}/share/themes/rose-pine.yaml";
+    base16Scheme = {
+      base00 = "#0f0f0f"; # default background
+      base01 = "#141414"; # lighter background (status bar)
+      base02 = "#212121"; # selection background
+      base03 = "#595959"; # comments, invisibles
+      base04 = "#737373"; # dark foreground (status bar)
+      base05 = "#fafafa"; # default foreground
+      base06 = "#ffffff"; # light foreground
+      base07 = "#c4c4c4"; # light background
+      base08 = "#ffb8e1"; # red (errors, deletion)
+      base09 = "#ffd190"; # orange
+      base0A = "#fff997"; # yellow (warnings, modification)
+      base0B = "#a7ffce"; # green (addition)
+      base0C = "#b2feff"; # cyan
+      base0D = "#ffffff"; # blue (keywords, functions)
+      base0E = "#d7aaff"; # magenta
+      base0F = "#c4c4c4"; # deprecated, secondary
+    };
 
-    # fonts.sizes = {
-    #   # applications = 12; # default: 12
-    #   # terminal = 12; # default: applications
-    #   # desktop = 8; # default: 10
-    #   # popups = 10; # default: desktop
-    # };
-
-    # opacity = {
-    #   applications = 0.95;
-    #   terminal = 0.95;
-    #   popups = 0.95;
-    #   desktop = 0.95;
-    # };
+    opacity = {
+      applications = 0.9;
+      terminal = 0.9;
+      popups = 0.9;
+      desktop = 0.9;
+    };
 
     icons = {
       enable = true;
