dd0e70553cbdf620f22000d7457d6f3aafb90d1f
jaycem <45575946+jaycem-dev@users.noreply.github.com>
2026-02-23 17:35:26 -0300

    chore: swaylock options

--- modules/home/swaylock.nix
+++ modules/home/swaylock.nix
@@ -8,6 +8,7 @@
     enable = true;
     package = if nixos then pkgs.swaylock else null;
     settings = {
+      ignore-empty-password = true;
       indicator-idle-visible = true;
       indicator-radius = 100;
       show-failed-attempts = true;
