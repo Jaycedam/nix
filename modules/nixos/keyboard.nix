a9dec5896dca4a44c5f443bb281c41b6e7e2e10b
jaycem <45575946+jaycem-dev@users.noreply.github.com>
2026-02-24 01:24:01 -0300

    chore: keyd mapping

--- modules/nixos/keyboard.nix
+++ modules/nixos/keyboard.nix
@@ -46,19 +46,20 @@
         };
 
         nav_layer = {
-          h = "left";
-          j = "down";
-          k = " up";
+          q = "esc";
+          h = "backspace";
+          j = "left";
+          k = "down";
           l = "right";
-          # forward word
-          w = "C-right";
-          # backward word
-          t = "C-left";
-          i = "backspace";
+          i = "up";
           ";" = "enter";
 
           f1 = "f1";
           f2 = "f2";
+          f3 = "f3";
+          f4 = "f4";
+          f5 = "f5";
+          f6 = "f6";
           f7 = "f7";
           f8 = "f8";
           f9 = "f9";
