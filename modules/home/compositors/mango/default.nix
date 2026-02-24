c16f38978849a250b81e062980143f8fd9f672e4
jaycem <45575946+jaycem-dev@users.noreply.github.com>
2026-02-23 21:39:19 -0300

    fix: mango display config

--- modules/home/compositors/mango/default.nix
+++ modules/home/compositors/mango/default.nix
@@ -12,7 +12,7 @@
       # monitor rules
       # monitorrule=name:Values,Parameter:Values,Parameter:Values
       monitorrule=name:eDP-1,scale:1.7
-      monitorrule=name:HDMI-A-1,refresh:72
+      monitorrule=name:HDMI-A-1,width:1920,height:1080,refresh:71.91
 
       # Window effect
       blur=1
