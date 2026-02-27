9b8f1e8b6ce26397f73c6260216d6045b494e9db
jaycem <45575946+jaycem-dev@users.noreply.github.com>
2026-02-27 19:46:35 -0300

    chore: extend stylix waybar css

--- modules/home/waybar/style.nix
+++ modules/home/waybar/style.nix
@@ -6,21 +6,11 @@
     }
 
     window#waybar {
-        background-color: @base00;
-        color: @base06;
         border-bottom: 1px solid @base02;
     }
 
     .module {
         padding: 0 8px;
-    }
-
-    tooltip {
-        background: @base00;
-        border: 1px solid @base0D ;
-    }
-    tooltip label {
-        color: @base00;
     }
 
     #bluetooth.connected, #network.wifi {
@@ -69,10 +59,37 @@
     #idle_inhibitor.activated {
         color: @base09;
     }
+
+    /* overrides for stylix */
+    .modules-left #workspaces button {
+        border-bottom: 0;
+    }
+    .modules-left #workspaces button.focused,
+    .modules-left #workspaces button.active {
+        border-bottom: 0;
+    }
+    .modules-left #workspaces button.urgent {
+        border-bottom: 0;
+    }
+    .modules-center #workspaces button {
+        border-bottom: 0;
+    }
+    .modules-center #workspaces button.focused,
+    .modules-center #workspaces button.active {
+        border-bottom: 0;
+    }
+    .modules-center #workspaces button.urgent {
+        border-bottom: 0;
+    }
+    .modules-right #workspaces button {
+        border-bottom: 0;
+    }
+    .modules-right #workspaces button.focused,
+    .modules-right #workspaces button.active {
+        border-bottom: 0;
+    }
+    .modules-right #workspaces button.urgent {
+        border-bottom: 0;
+    }
   '';
-
-  stylix.targets.waybar = {
-    addCss = false;
-  };
-
 }
