5ae4587a79f942bba97e404e1c235027024ef8e1
jaycem <45575946+jaycem-dev@users.noreply.github.com>
2026-02-26 22:28:32 -0300

    feat(greeter): autologin logic per compositor

--- modules/nixos/greeter.nix
+++ modules/nixos/greeter.nix
@@ -1,15 +1,28 @@
 {
   pkgs,
   config,
+  compositor,
+  user,
   ...
 }:
 {
   services.greetd = {
     enable = true;
+    useTextGreeter = true;
     settings = {
+      initial_session = {
+        inherit user;
+        command =
+          if compositor == "niri" then
+            "niri-session"
+          else if compositor == "mango" then
+            "exec mango"
+          else
+            throw "Unsupported compositor: ${compositor}";
+      };
       default_session = {
-        command = "${pkgs.tuigreet}/bin/tuigreet --time --asterisks --remember --remember-session --sessions ${config.services.displayManager.sessionData.desktops}/share/wayland-sessions";
         user = "greeter";
+        command = "${pkgs.tuigreet}/bin/tuigreet --time --asterisks --remember --remember-session --sessions ${config.services.displayManager.sessionData.desktops}/share/wayland-sessions";
       };
     };
   };
