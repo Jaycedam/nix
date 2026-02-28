1ace8e20a0d4bbc9aa13fc5809b0351b3130df81
jaycem <45575946+jaycem-dev@users.noreply.github.com>
2026-02-28 14:32:45 -0300

    chore: stylix themes vars

--- modules/common/stylix.nix
+++ modules/common/stylix.nix
@@ -1,27 +1,34 @@
 { pkgs, ... }:
+let
+  themes = {
+    rose-pine = "${pkgs.base16-schemes}/share/themes/rose-pine.yaml";
+    kanagawa = "${pkgs.base16-schemes}/share/themes/kanagawa.yaml";
+    catppuccin-mocha = "${pkgs.base16-schemes}/share/themes/catppuccin-mocha.yaml";
+    black = {
+      base00 = "#0f0f0f"; # Default Background
+      base01 = "#181818"; # Lighter Background (Used for status bars, line number and folding marks)
+      base02 = "#252525"; # Selection Background
+      base03 = "#595959"; # Comments, Invisibles, Line Highlighting
+      base04 = "#737373"; # Dark Foreground (Used for status bars)
+      base05 = "#fafafa"; # Default Foreground, Caret, Delimiters, Operators
+      base06 = "#ffffff"; # Light Foreground (Not often used)
+      base07 = "#c4c4c4"; # Light Background (Not often used)
+      base08 = "#ffb8e1"; # Variables, XML Tags, Markup Link Text, Markup Lists, Diff Deleted
+      base09 = "#ffd190"; # Integers, Boolean, Constants, XML Attributes, Markup Link Url
+      base0A = "#fff997"; # Classes, Markup Bold, Search Text Background
+      base0B = "#a7ffce"; # Strings, Inherited Class, Markup Code, Diff Inserted
+      base0C = "#b2feff"; # Support, Regular Expressions, Escape Characters, Markup Quotes
+      base0D = "#ffffff"; # Functions, Methods, Attribute IDs, Headings
+      base0E = "#d7aaff"; # Keywords, Storage, Selector, Markup Italic, Diff Changed
+      base0F = "#c4c4c4"; # Deprecated, Opening/Closing Embedded Language Tags, e.g. `<?php ?>`
+    };
+  };
+in
 {
   stylix = {
     enable = true;
     polarity = "dark";
-    # base16Scheme = "${pkgs.base16-schemes}/share/themes/rose-pine.yaml";
-    base16Scheme = {
-      base00 = "#0f0f0f"; # default background
-      base01 = "#161616"; # lighter background (status bar)
-      base02 = "#222222"; # selection background
-      base03 = "#595959"; # comments, invisibles
-      base04 = "#737373"; # dark foreground (status bar)
-      base05 = "#fafafa"; # default foreground
-      base06 = "#ffffff"; # light foreground
-      base07 = "#c4c4c4"; # light background
-      base08 = "#ffb8e1"; # red (errors, deletion)
-      base09 = "#ffd190"; # orange
-      base0A = "#fff997"; # yellow (warnings, modification)
-      base0B = "#a7ffce"; # green (addition)
-      base0C = "#b2feff"; # cyan
-      base0D = "#ffffff"; # blue (keywords, functions)
-      base0E = "#d7aaff"; # magenta
-      base0F = "#c4c4c4"; # deprecated, secondary
-    };
+    base16Scheme = themes.rose-pine;
 
     opacity = {
       applications = 0.9;
