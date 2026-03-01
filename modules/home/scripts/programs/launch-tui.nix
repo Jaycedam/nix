c7b39595435b2e5e63f043e3572227f6da5446af
jaycem <45575946+jaycem-dev@users.noreply.github.com>
2026-03-01 19:31:16 -0300

    chore: fix script

--- modules/home/scripts/programs/launch-tui.nix
+++ modules/home/scripts/programs/launch-tui.nix
@@ -5,8 +5,7 @@ let
 in
 
 pkgs.writeShellScriptBin "launch-tui" ''
-  app="''${1:?Usage: launch-tui <app>} [args]"
+  app="''${1:?Usage: launch-tui <app>}"
   shift
-  echo "Running: ${kitty} -1 --class $app $app $*"
   ${kitty} -1 --class $app $app $*
 ''
