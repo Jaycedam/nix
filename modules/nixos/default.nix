094ea400c39baf85a5ffc2227cf03e98c6c96711
jaycem <45575946+jaycem-dev@users.noreply.github.com>
2026-03-05 19:34:53 -0300

    check for system instead of apple-silicon for gaming.nix

--- modules/nixos/default.nix
+++ modules/nixos/default.nix
@@ -1,8 +1,8 @@
 {
   compositor,
-  apple-silicon ? null,
   lib,
   desktop,
+  system,
   ...
 }:
 {
@@ -12,28 +12,30 @@
     "/share/xdg-desktop-portal"
   ];
 
-  imports = [
-    ./options.nix
-    ./users.nix
-    ./boot.nix
-    ./network.nix
-    ./services.nix
-    ./chromium.nix
-    ./keyboard.nix
-    ./greeter.nix
-    ./virtualization.nix
-    ../common/stylix.nix
-  ]
-  ++ lib.optional desktop ./homelab.nix
-  ++ lib.optionals (apple-silicon == null) [
-    (
-      if compositor == "niri" then
-        ./niri.nix
-      else if compositor == "mango" then
-        ./mango.nix
-      else
-        throw "Unsupported compositor: ${compositor}"
-    )
-    ./gaming.nix
-  ];
+  imports =
+    [
+      ./options.nix
+      ./users.nix
+      ./boot.nix
+      ./network.nix
+      ./services.nix
+      ./chromium.nix
+      ./keyboard.nix
+      ./greeter.nix
+      ./virtualization.nix
+      ../common/stylix.nix
+    ]
+      (
+        if compositor == "niri" then
+          ./niri.nix
+        else if compositor == "mango" then
+          ./mango.nix
+        else
+          null
+      )
+    ++ lib.optional desktop ./homelab.nix
+    # don't enable gaming on arm, it requires 32bit support
+    ++ lib.optionals (system == "x86_64-linux") [
+      ./gaming.nix
+    ];
 }
