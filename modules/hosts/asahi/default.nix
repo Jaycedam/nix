95f04399215070e57f6c8903aeeb0bfe1cccf5ff
jaycem <45575946+jaycem-dev@users.noreply.github.com>
2026-03-05 00:40:24 -0300

    zram, swap and fix efivariables bool

--- modules/hosts/asahi/default.nix
+++ modules/hosts/asahi/default.nix
@@ -1,9 +1,18 @@
-_: {
+{ lib, ... }:
+{
   imports = [
     ./hardware-configuration.nix
   ];
 
+  boot.loader.efi.canTouchEfiVariables = lib.mkDefault false;
   hardware.bluetooth.enable = true;
+  zramSwap.enable = true;
+  swapDevices = [
+    {
+      device = "/var/lib/swapfile";
+      size = 8 * 1024;
+    }
+  ];
 
   networking.hostName = "asahi";
   system.stateVersion = "25.05";
