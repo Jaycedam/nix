35115fa374bd7ac2b12779534e273e2349608639
jaycem <45575946+jaycem-dev@users.noreply.github.com>
2026-03-06 19:07:17 -0300

    stylix defaults

--- modules/nixos/stylix.nix
+++ modules/nixos/stylix.nix
@@ -1,6 +1,7 @@
 {
   inputs,
   theme,
+  pkgs,
   ...
 }:
 {
@@ -8,4 +9,38 @@
     inputs.stylix.nixosModules.stylix
     ../themes/${theme.name}.nix
   ];
+
+  stylix = {
+    enable = true;
+    opacity = {
+      applications = 0.9;
+      terminal = 0.9;
+      popups = 0.9;
+      desktop = 0.9;
+    };
+
+    cursor.size = 24;
+
+    fonts = {
+      serif = {
+        package = pkgs.dejavu_fonts;
+        name = "DejaVu Serif";
+      };
+
+      sansSerif = {
+        package = pkgs.dejavu_fonts;
+        name = "DejaVu Sans";
+      };
+
+      monospace = {
+        package = pkgs.jetbrains-mono;
+        name = "JetBrains Mono";
+      };
+
+      emoji = {
+        package = pkgs.noto-fonts-color-emoji;
+        name = "Noto Color Emoji";
+      };
+    };
+  };
 }
