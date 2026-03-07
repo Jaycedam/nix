e52fc6567c98a94951ef222eb387d11cef68b0bc
jaycem <45575946+jaycem-dev@users.noreply.github.com>
2026-03-06 21:51:45 -0300

    fix(asahi): display backlight control not working
    The driver doesn't load correctly on boot, until a display power cycle.
    This is an ugly workaraound until a real fix, that uses the compositor's
    ipc to power on/off the display.

--- modules/home/programs/niri/niri.nix
+++ modules/home/programs/niri/niri.nix
@@ -1,10 +1,26 @@
 {
   config,
+  pkgs,
   ...
 }:
 
 {
   home = {
     file."${config.xdg.configHome}/niri/config.kdl".source = ./niri.kdl;
+  };
+
+  # workaraound for Apple backlight not working on boot
+  systemd.user.services.fix-apple-backlight = {
+    Unit = {
+      Description = "Power cycle display to fix Apple backlight on boot";
+      After = [ "graphical.target" ];
+    };
+    Service = {
+      Type = "oneshot";
+      ExecStart = "${pkgs.bash}/bin/bash -c 'niri msg action power-off-monitors; sleep 1; niri msg action power-on-monitors'";
+    };
+    Install = {
+      WantedBy = [ "graphical.target" ];
+    };
   };
 }
