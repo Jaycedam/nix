e6ba3dd5cf8eec41786d00d669200c53253b49f4
jaycem <45575946+jaycem-dev@users.noreply.github.com>
2026-03-09 22:49:25 -0300

    fix niri format dir

--- modules/home/programs/niri/default.nix
+++ modules/home/programs/niri/default.nix
@@ -1,5 +1,46 @@
-_: {
-  imports = [
-    ./niri.nix
-  ];
+{
+  config,
+  pkgs,
+  lib,
+  theme,
+  ...
+}:
+{
+  home = {
+    file."${config.xdg.configHome}/niri/config.kdl".source = pkgs.writeText "config.kdl" (
+      lib.replaceStrings
+        [
+          "__ACTIVE_COLOR__"
+          "__INACTIVE_COLOR__"
+          "__BORDER_RADIUS__"
+        ]
+        [
+          config.lib.stylix.colors.base0D
+          config.lib.stylix.colors.base03
+          (builtins.toString theme.borderRadius)
+
+        ]
+        (builtins.readFile ./niri.kdl)
+    );
+  };
+
+  # workaraound for Apple backlight not working on boot
+  systemd.user.services.fix-apple-backlight = {
+    Unit = {
+      Description = "Power cycle display to fix Apple backlight on boot";
+      After = [ "graphical-session.target" ];
+    };
+    Service = {
+      Type = "oneshot";
+      ExecStart = pkgs.writeShellScript "fix-apple-backlight.sh" ''
+        export PATH="${pkgs.coreutils}/bin:${pkgs.bash}/bin"
+        ${pkgs.niri}/bin/niri msg output eDP-1 off
+        sleep 1
+        ${pkgs.niri}/bin/niri msg output eDP-1 on
+      '';
+    };
+    Install = {
+      WantedBy = [ "graphical-session.target" ];
+    };
+  };
 }
