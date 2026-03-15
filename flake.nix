b0dae0ec64e104245fcfc085f51b6adfbd34bc22
jaycem <45575946+jaycem-dev@users.noreply.github.com>
2026-03-15 14:29:32 -0300

    change default compositor

--- flake.nix
+++ flake.nix
@@ -73,24 +73,24 @@
       profiles = {
         desktop = {
           host = "desktop";
-          compositor = compositors.niri;
-        };
-        desktop-mango = {
-          host = "desktop";
           compositor = compositors.mango;
+        };
+        desktop-niri = {
+          host = "desktop";
+          compositor = compositors.niri;
         };
 
         asahi = {
           host = "asahi";
           system = systems.linux-arm;
           desktop = false;
-          compositor = compositors.niri;
+          compositor = compositors.mango;
         };
-        asahi-mango = {
+        asahi-niri = {
           host = "asahi";
           system = systems.linux-arm;
           desktop = false;
-          compositor = compositors.mango;
+          compositor = compositors.niri;
         };
 
       };
