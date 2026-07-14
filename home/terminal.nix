18969e70dafee7633677c667c5ca3357565b5afc
Jay <45575946+jaycem-dev@users.noreply.github.com>
2026-07-14 00:34:08 -0400

    fix nix opts

--- home/terminal.nix
+++ home/terminal.nix
@@ -1,33 +1,35 @@
 _: {
-  kitty = {
-    enable = true;
-    enableGitIntegration = true;
-    shellIntegration.enableFishIntegration = true;
-    settings = {
-      cursor_trail = 1;
-      tab_bar_edge = "top";
-      background_blur = 1;
+  programs = {
+    kitty = {
+      enable = true;
+      enableGitIntegration = true;
+      shellIntegration.enableFishIntegration = true;
+      settings = {
+        cursor_trail = 1;
+        tab_bar_edge = "top";
+        background_blur = 1;
+      };
+      shell = {
+        program = "fish";
+      };
+      actionAliases = {
+        launch_window = "launch --cwd current --type os-window";
+        launch_overlay = "launch --cwd current --type overlay";
+      };
+      keybindings = {
+        "kitty_mod+n" = "launch_window";
+        "kitty_mod+g" = "launch_window --os-window-class git lazygit";
+        "kitty_mod+a" = "launch_window --os-window-class opencode opencode";
+      };
     };
-    shell = {
-      program = "fish";
-    };
-    actionAliases = {
-      launch_window = "launch --cwd current --type os-window";
-      launch_overlay = "launch --cwd current --type overlay";
-    };
-    keybindings = {
-      "kitty_mod+n" = "launch_window";
-      "kitty_mod+g" = "launch_window --os-window-class git lazygit";
-      "kitty_mod+a" = "launch_window --os-window-class opencode opencode";
-    };
-  };
 
-  ghostty = {
-    enable = true;
-    settings = {
-      mouse-hide-while-typing = "true";
-      command = "fish";
+    ghostty = {
+      enable = true;
+      settings = {
+        mouse-hide-while-typing = "true";
+        command = "fish";
+      };
     };
-  };
 
+  };
 }
