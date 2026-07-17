61b5565e4210581ad764fb50f2b809384b77598c
Jay <45575946+jaycem-dev@users.noreply.github.com>
2026-07-17 02:17:40 -0400

    use xdg.configFile instead of home.file

--- home/browser.nix
+++ home/browser.nix
@@ -20,7 +20,7 @@
   };
 
   # Widevine CDM for aarch64 (DRM support)
-  home.file.".config/BraveSoftware/Brave-Browser/WidevineCdm/latest-component-updated-widevine-cdm" =
+  xdg.configFile."BraveSoftware/Brave-Browser/WidevineCdm/latest-component-updated-widevine-cdm" =
     lib.mkIf (system == "aarch64-linux")
       {
         text = ''{"Path":"${pkgs.widevine-cdm}/share/google/chrome/WidevineCdm"}'';
