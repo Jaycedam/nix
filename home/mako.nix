eb80452151a216a332bdfe3d602126e2089ec528
Jay <45575946+jaycem-dev@users.noreply.github.com>
2026-07-27 23:18:56 -0400

    update lockfile and small changes

--- home/mako.nix
+++ home/mako.nix
@@ -11,7 +11,7 @@
       width = 400;
       padding = 10;
       border-size = 2;
-      inherit (theme) border-radius;
+      border-radius = theme.border-radius;
       layer = "overlay";
       # set opacity to 1 until ext-background-effect-v1 is added https://github.com/emersion/mako/issues/631
       background-color = lib.mkForce (config.lib.stylix.colors.withHashtag.base00 + "FF");
