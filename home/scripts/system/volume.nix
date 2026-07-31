5e2fd3e088148fb5ee05dbf602853647b487e3bc
Jay <45575946+jaycem-dev@users.noreply.github.com>
2026-07-30 23:53:00 -0400

    improve volume script

--- home/scripts/system/volume.nix
+++ home/scripts/system/volume.nix
@@ -46,40 +46,40 @@ pkgs.writeShellApplication {
         exit 1
     fi
 
-    CURRENT_VOL=$(wpctl get-volume @DEFAULT_AUDIO_SINK@ | awk '{print $2}')
-    CURRENT_PERCENT=$(awk "BEGIN {printf \"%.0f\", $CURRENT_VOL * 100}")
-    ICON=""
+    notify() {
+        local target=$1
+        local muted_icon=$2
+        local icon=$3
+        local vol percent output
 
-    if [ "$1" = "mute" ]; then
-        wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle
-        if wpctl get-volume @DEFAULT_AUDIO_SINK@ | grep -q MUTED; then
-            NEW_PERCENT=0
-            ICON=""
-        else
-            NEW_PERCENT=$CURRENT_PERCENT
-            ICON=""
+        output=$(wpctl get-volume "$target")
+        vol=$(printf '%s' "$output" | awk '{print $2}')
+        percent=$(awk "BEGIN {printf \"%.0f\", $vol * 100}")
+
+        if printf '%s' "$output" | grep -q MUTED; then
+            percent=0
+            icon=$muted_icon
         fi
-    elif [ "$1" = "micmute" ]; then
-        wpctl set-mute @DEFAULT_AUDIO_SOURCE@ toggle
-        MIC_VOL=$(wpctl get-volume @DEFAULT_AUDIO_SOURCE@ | awk '{print $2}')
-        MIC_PERCENT=$(awk "BEGIN {printf \"%.0f\", $MIC_VOL * 100}")
-        if wpctl get-volume @DEFAULT_AUDIO_SOURCE@ | grep -q MUTED; then
-            NEW_PERCENT=0
-            ICON="󰍭"
-        else
-            NEW_PERCENT=$MIC_PERCENT
-            ICON="󰍬"
-        fi
-    fi
+
+        notify-send -c osd -h string:x-canonical-private-synchronous:osd -h int:value:"$percent" "$icon $percent%"
+    }
+
+    VOLUME_ICON=""
+    MUTED_ICON=""
+    MIC_MUTED_ICON="󰍭"
+    MIC_ICON="󰍬"
 
     if [ "$1" = "up" ] || [ "$1" = "down" ]; then
-        wpctl set-mute @DEFAULT_AUDIO_SINK@ 0
         [ "$1" = "down" ] && dir="-" || dir="+"
+        wpctl set-mute @DEFAULT_AUDIO_SINK@ 0
         wpctl set-volume @DEFAULT_AUDIO_SINK@ "''${step}%''${dir}" -l 1.0
-        NEW_VOL=$(wpctl get-volume @DEFAULT_AUDIO_SINK@ | awk '{print $2}')
-        NEW_PERCENT=$(awk "BEGIN {printf \"%.0f\", $NEW_VOL * 100}")
+        notify @DEFAULT_AUDIO_SINK@ "$MUTED_ICON" "$VOLUME_ICON"
+    elif [ "$1" = "mute" ]; then
+        wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle
+        notify @DEFAULT_AUDIO_SINK@ "$MUTED_ICON" "$VOLUME_ICON"
+    elif [ "$1" = "micmute" ]; then
+        wpctl set-mute @DEFAULT_AUDIO_SOURCE@ toggle
+        notify @DEFAULT_AUDIO_SOURCE@ "$MIC_MUTED_ICON" "$MIC_ICON"
     fi
-
-    notify-send -c osd -h string:x-canonical-private-synchronous:osd -h int:value:"$NEW_PERCENT" "$ICON $NEW_PERCENT%"
   '';
 }
