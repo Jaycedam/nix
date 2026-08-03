b56bafb99ee94de426291d804fb5f6f903d482bf
Jay <45575946+jaycem-dev@users.noreply.github.com>
2026-08-03 00:01:10 -0400

    migrate niri config to home-manager module

---
+++ home/niri/colors.nix
@@ -0,0 +1,33 @@
+{ config, ... }:
+{
+  wayland.windowManager.niri.settings = with config.lib.stylix.colors.withHashtag; {
+    layout = {
+      background-color = base00;
+      focus-ring = {
+        off = { };
+        active-color = base0D;
+        inactive-color = base01;
+        urgent-color = base08;
+      };
+      border = {
+        on = { };
+        width = 3;
+        active-color = base0D;
+        inactive-color = base01;
+        urgent-color = base08;
+      };
+      shadow.color = "${base00}70";
+      tab-indicator = {
+        active-color = base0D;
+        inactive-color = base01;
+        urgent-color = base08;
+      };
+      insert-hint.color = "${base0C}80";
+    };
+    recent-windows.highlight = {
+      active-color = base0D;
+      urgent-color = base08;
+    };
+    overview.backdrop-color = base00;
+  };
+}
